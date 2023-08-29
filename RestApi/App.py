a8f480585d7475c00a9359613b1d4fb6a22c3971
unknown <jpmurdolo@gmail.com>
2023-08-29 14:36:00 -0300

    directory structure and app.py

---
+++ RestApi/App.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask,request
+from flask_smorest import Api, Blueprint, abort
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["API_TITLE"] = "PF Coding Challenge"
+    app.config["API_VERSION"] = "v0.1"
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["OPENAPI_VERSION"] = "3.0.2"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.24.2/"
+    api = Api(app)
+
+    return app
