0cb62a0edb2808420afabd01f0dc62f514f99a95
unknown <jpmurdolo@gmail.com>
2023-09-04 11:45:03 -0300

    schemas comments

--- RestApi/schemas.py
+++ RestApi/schemas.py
@@ -1,15 +1,18 @@
 from marshmallow import Schema, fields, validate
 
 class PlainChartDataSchema(Schema):
+    #The base information of a Char_Data based on the Json file received
     sprocket_production_goal = fields.Int()
     sprocket_production_rate = fields.Int()
     time = fields.Int()
 
 class PlainFactorySchema(Schema):
+    #The factory had to have an ID to be able to search for it in the GET endpoint
     id = fields.Int(dump_only=True)
     name = fields.Str(validate=validate.Length(min=1, max=50))
 
 class PlainSprocketSchema(Schema):
+    #The base information of a sprocket based on the Json file received
     id = fields.Int(dump_only=True)
     teeth = fields.Int()
     pitch_diameter = fields.Int()
@@ -17,23 +20,27 @@ class PlainSprocketSchema(Schema):
     pitch = fields.Int()
 
 class SprocketSchema(PlainSprocketSchema):
+    #This is a custom thing I made to be able to define which factory is producing which sprocket
     factory_id = fields.Int(load_only=True, nullable=True)
     factory = fields.Nested(PlainFactorySchema(), dump_only=True, load_only=True, nullable=True)
 
 class ChartDataSchema(PlainChartDataSchema):
+    #The chart data is related to the factory
     factory_id = fields.Int(dump_only=True, load_only=True)
     factory = fields.Nested(PlainFactorySchema(), dump_only=True, load_only=True)
 
 class FactorySchema(PlainFactorySchema):
-    #sprockets only works for knowning which sprockets are being produced in the factory
+    #This add the value of the sprockets and the chart data schemas I defined before this to the factory
     sprockets = fields.List(fields.Nested(SprocketSchema(), dump_only=True, default=[]))
     chart_data = fields.List(fields.Nested(ChartDataSchema(), dump_only=True, default=[]))
 
 class FactorySprocketSchema(Schema):
+    #This is a custom thing I made, but in this case is defined since the relationship between factories and sprockets is many-to-many
     factory = fields.Nested(PlainFactorySchema())
     sprocket = fields.Nested(PlainSprocketSchema())
 
 class FactoryUpdateSchema(Schema):
+    #The only value that can be updated to a factory on its own is the name
     name = fields.Str(validate=validate.Length(min=1, max=50), required=False)
 '''
 I would have added an order schema and a stock control for the sprockets so it would be something like this
